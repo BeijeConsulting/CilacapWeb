e1d978e0fee02c939e2a5d422386e6d7d6c26f21
Padawan10 <Padawan10@DESKTOP-JP18TAH>
2020-02-11 12:38:14 +0100

    Commit before merge 1102

---
+++ src/it/beije/cilacap/util/HDBtools.java
@@ -0,0 +1,132 @@
+package it.beije.cilacap.util;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+//import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+//import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
+
+
+public class HDBtools {
+	
+	private static Log logger = LogFactory.getLog(HDBtools.class);
+	
+	public static List<Contatto> getContactFromHDB() {
+		
+		logger.debug("INIZIO");
+		
+		//Configuration opener 
+		Configuration configuration = new Configuration();
+		configuration = configuration.configure("it/beije/cilacap/rubrica/hibernate.cfg3.xml")
+				.addAnnotatedClass(Contatto.class); // Specification of the used object
+		
+		//Session generator 
+		SessionFactory factory = configuration.buildSessionFactory();
+		System.out.println("Is it open? " + factory.isOpen());
+		
+		//Session opener
+		Session session = factory.openSession();
+		
+		//Creation of the string with command in HQL dialect
+		String hql = "SELECT c FROM Contatto as c";
+		
+		//Query creation
+		Query<Contatto> query = session.createQuery(hql);
+		
+		return query.list();
+		
+	}
+	
+	public static void insertInHDB(List<Contatto> contatti) {
+		
+		logger.debug("INIZIO");
+		
+		//Configuration opener 
+		Configuration configuration = new Configuration();
+		configuration = configuration.configure("it/beije/cilacap/rubrica/hibernate.cfg3.xml")
+				.addAnnotatedClass(Contatto.class); // Specification of object used
+		
+		//Session generator 
+		SessionFactory factory = configuration.buildSessionFactory();
+		System.out.println("Is it open? " + factory.isOpen());
+		
+		//Session opener
+		Session session = factory.openSession();	
+		
+		//Transaction opener
+		Transaction transaction = session.beginTransaction();
+		
+		//Example INSERT
+		Contatto contatto = new Contatto();
+		contatto.setNome("Marco");
+		contatto.setCognome("Polo");
+		contatto.setEmail("marco@polo.it");
+		contatto.setTelefono("34553567");
+		session.save(contatto);
+		
+		//Saving each contact
+//		for (Contatto c : contatti) {
+//			session.save(c);
+//		}
+		
+		//Confirmation of insertion in DB	
+		transaction.commit();
+		
+		//Session Closer
+		session.close();
+		System.out.println("Is session open? " + session.isOpen());
+		factory.close();
+		
+		logger.debug("FINE");
+		
+	}
+	
+	public static void updateHDB(List<Contatto> contatti) {
+		
+		logger.debug("INIZIO");
+		
+		//Configuration opener 
+		Configuration configuration = new Configuration();
+		configuration = configuration.configure("it/beije/cilacap/rubrica/hibernate.cfg3.xml")
+				.addAnnotatedClass(Contatto.class); // Specification of object used
+		
+		//Session generator 
+		SessionFactory factory = configuration.buildSessionFactory();
+		System.out.println("Is it open? " + factory.isOpen());
+		
+		//Session opener
+		Session session = factory.openSession();	
+		
+		//Transaction opener
+		Transaction transaction = session.beginTransaction();
+		
+		//Example UPDATE
+		Contatto contatto = session.get(Contatto.class, 3);
+		System.out.println(contatto);
+		contatto.setTelefono("777777777");
+		System.out.println(contatto);
+		session.save(contatto);
+		
+		
+		//Confirm of update in DB
+		transaction.commit();
+		
+		//Cancel update in DB
+//		transaction.rollback();
+		
+		//Session close
+		session.close();
+
+		System.out.println("Is session open? " + session.isOpen());
+
+		logger.debug("FINE");
+
+	}
+	
+}
---
+++ src/it/beije/cilacap/web/TestServlet2.java
@@ -0,0 +1,52 @@
+package it.beije.cilacap.web;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test")
+public class TestServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//File file = new File();
+		
+		
+		
+		
+//		Contatto contatto = new Contatto();
+		LocalTime now = LocalTime.now();
+		String message = now.isBefore(LocalTime.of(16, 00)) ? "BUONGIORNO" : "BUONASERA";
+		System.out.println("sono un output");
+		
+		StringBuilder builder = new StringBuilder("<!DOCTYPE html><html><head><title>CILACAP</title></head>");
+		builder.append("<body><b>").append(message).append(" CILACAP").append("</b></body></html>");
+		builder.append("<img src=https://htpwj-zone1-rrzzqutxbqkmxn.netdna-ssl.com/wp-content/uploads/2012/09/Java-Podcast.jpg />");  
+		//response.setContentType("text/html");
+		response.getWriter().append(builder.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
