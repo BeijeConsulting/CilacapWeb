e05c430a04c0bf36c6896d0e82181c0788e13401
Padawan10 <Padawan10@DESKTOP-JP18TAH>
2020-02-13 15:04:12 +0100

    Commit 1302

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -8,18 +8,25 @@
 </head>
 <body>
 
-<%! String username = "es. Gabriele"; %>
+<%! String username = "es. Gabriele";
+	String cognome = "es. Capobianco";
+	String telefono = "es. 00000000";
+	String email = "es. Gabriele@beije.com";
+%>
 
 <h1>LOGIN PAGE</h1>
 <h6>Cilacap</h6>
 
 <p>inserisci di seguito i tuoi dati</p>
 
-<form action="test" method="POST">
+<form action="rubrica" method="POST">
   Username: <input type="text" name="username" placeholder="<%= username %>"><br><br>
-  Password: <input type="password" name="password"><br><br>
+  Cognome: <input type="cognome" name="cognome" placeholder="<%= cognome %>"><br><br>
+  Telefono: <input type="telefono" name="telefono" placeholder="<%= telefono %>"><br><br>
+  Email: <input type="email" name="email" placeholder="<%= email %>"><br><br>
   <input type="submit" value="INVIA">
 </form>
+
 
 </body>
 </html>
---
+++ src/it/beije/cilacap/web/ServletRubrica.java
@@ -0,0 +1,77 @@
+package it.beije.cilacap.web;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.beije.cilacap.util.Contatto;
+import it.beije.cilacap.util.DBtools;
+
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/rubrica")
+public class ServletRubrica extends HttpServlet {
+	private static final long serialVersionUID = 2L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		StringBuilder builder = new StringBuilder("<!DOCTYPE html><html><head><title>CILACAP</title></head>");
+		builder.append("<body><b>").append(" CILACAP").append("</b></body></html>");
+		
+		//response.setContentType("text/html");
+		response.getWriter().append(builder.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String cognome = request.getParameter("cognome");
+		String telefono = request.getParameter("telefono");
+		String email = request.getParameter("email");
+		
+		List<Contatto> contacts = new ArrayList<Contatto>();
+		Contatto contatto = new Contatto();
+    	contatto.setNome(username);
+    	contatto.setCognome(cognome);
+    	contatto.setTelefono(telefono);
+    	contatto.setEmail(email);
+    	contacts.add(contatto);
+    	
+		System.out.println("username : " + username);
+		System.out.println("cognome : " + cognome);
+		System.out.println("telefono : " + telefono);
+		System.out.println("email : " + email);
+		
+		response.setContentType("text/html");
+		response.getWriter().append("username : ").append(username).append("<br>")
+			.append("cognome : ").append(cognome).append("<br>").append("telefono: ")
+			.append(telefono).append("<br>").append("email: ").append(email).append("<br>");
+		
+		
+		try {
+			
+			DBtools.insertContatto(contacts);
+			
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+
+}
