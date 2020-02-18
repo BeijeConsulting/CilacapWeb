004de5c487c6ebf52d0eb2791286122adc4ee87c
Padawan02 <Padawan02@DESKTOP-I6EQIGK>
2020-02-18 12:43:53 +0100

    c

--- src/it/beije/cilacap/web/User.java
+++ src/it/beije/cilacap/web/User.java
@@ -6,9 +6,23 @@ public class User {
 	private String username;
 	private String firstName;
 	private String lastName;
+	private String telefono;
+	private String email;
 	
 	public Integer getId() {
 		return id;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public void setId(Integer id) {
 		this.id = id;
