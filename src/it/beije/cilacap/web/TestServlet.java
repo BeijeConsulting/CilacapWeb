f20ea53045cb5df7fb0888b97ada1664c6e4d4c4
Padawan04 <Padawan04@DESKTOP-LV5RKMD>
2020-03-02 13:28:42 +0100

    Padawan04 commit sporco

--- src/it/beije/cilacap/web/TestServlet.java
+++ src/it/beije/cilacap/web/TestServlet.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/masterSpringMVC
 /**
  * Servlet implementation class TestServlet
  */
