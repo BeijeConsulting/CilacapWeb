5377171b6ce0ed7be5d667db394edf0e8ce93c51
Padawan04 <Padawan04@DESKTOP-LV5RKMD>
2020-03-02 11:52:33 +0100

    push per congelare mie modifiche

--- src/it/beije/cilacap/controller/HomeController.java
+++ src/it/beije/cilacap/controller/HomeController.java
@@ -14,7 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import it.beije.cilacap.web.rubrica.Contatto;
 
