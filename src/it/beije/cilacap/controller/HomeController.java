62afd0a4e7140d111e11dac0868b0dff655e3392
Clark <clarke2897@gmail.com>
2020-03-02 12:35:00 +0100

    sistemato progetto, pom.xml

--- src/it/beije/cilacap/controller/HomeController.java
+++ src/it/beije/cilacap/controller/HomeController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import it.beije.cilacap.web.rubrica.Contatto;
+import it.beije.cilacap.web.rubrica.model.Contatto;
+
 
 
 @Controller
